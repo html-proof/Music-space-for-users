996e82a6ac9194315a3537f2c73714111519b3d3
Sebastian Thomas <68529971+html-proof@users.noreply.github.com>
2026-08-24 20:35:08 +0530

    Delete mobile_app directory

--- mobile_app/lib/features/search/application/search_providers.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../../../core/providers/core_providers.dart';
-import '../data/search_repository.dart';
-
-final searchRepositoryProvider = Provider<SearchRepository>((ref) {
-  return SearchRepository(ref.watch(apiClientProvider));
-});
